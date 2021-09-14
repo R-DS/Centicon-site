df066802d91d8b86a663977b8e0e2db7f47d6595
R-DS <52845913+R-DS@users.noreply.github.com>
2021-09-15 00:57:17 +1000

    My First Commit

---
+++ centicon.js
@@ -0,0 +1,105 @@
+
+
+
+function myFunction() {
+    const dots = document.getElementById("dots");
+    const moreText = document.getElementById("more");
+    const btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+
+
+let items = document.querySelectorAll('.abt')
+
+items.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+});
+
+
+/*navbar javascript*/
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    el_autohide = document.querySelector('.autohide');
+
+    // add padding-top to bady (if necessary)
+    navbar_height = document.querySelector('.navbar').offsetHeight;
+    document.body.style.paddingTop = navbar_height + 'px';
+
+    if(el_autohide){
+        var last_scroll_top = 0;
+        window.addEventListener('scroll', function() {
+            let scroll_top = window.scrollY;
+            if(scroll_top < last_scroll_top) {
+                el_autohide.classList.remove('scrolled-down');
+                el_autohide.classList.add('scrolled-up');
+            }
+            else {
+                el_autohide.classList.remove('scrolled-up');
+                el_autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+        });
+        // window.addEventListener
+    }
+    // if
+
+});
+
+
+// Navbar toggler collapse automatically after clicked
+
+//
+
+/* navbar js ends*/
+
+
+
+/*Home and Contact animation*/
+window.sr = ScrollReveal();
+sr.reveal('.Home', {duration: 3000, origin: 'top', distance: '40px'});
+sr.reveal('.contactleft', {
+    duration: 2000, origin: 'left', distance: '300px'
+});
+sr.reveal('.contactright', {
+    duration: 2000, origin: 'right', distance: '300px'
+});
+
+
+/*Scrolling smooth animation*/
+/*
+$(function () {
+    // smooth scrolling
+    $('a[href*="#"]:not([href="#"])').click(function (){
+        if(location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') &&
+            location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});*/
